ae900448d3e9f9cbcd68b3da15e76b4538c61ca0
schougu5 <snehal.chougule04@gmail.com>
2022-10-12 10:43:32 +0530

    Java file content changed

--- HelloSecond.java
+++ HelloSecond.java
@@ -1,6 +1,10 @@
-public class HelloSecond {
-    public static void main(String[] args) {
-        System.out.println("Om Arham Namah...");
-    }
 
-}
+:
+:q
+:wq
+exit
+
+Exit
+clear
+close
+Exit
